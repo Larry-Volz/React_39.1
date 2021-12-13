58f69c667899313067396947541a491eff70c7e3
Larry Volz <imaginologist@gmail.com>
2021-12-12 20:57:51 -0500

    tried mutating props a different way and it still seems mutable

--- react-intro-demo/codingAlong/RandomChoice.js
+++ react-intro-demo/codingAlong/RandomChoice.js
@@ -1,6 +1,7 @@
 const RandomChoice = (props) => {
     const idx = Math.floor(Math.random() * props.choices.length);
     console.log(`idx is: ${idx}`)
+    // props.choices[idx]=1000;
     let choice = props.choices[idx];
     // choice = 1000;
     return <h4>Random Choice is: {choice} </h4>
